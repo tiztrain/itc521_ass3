e2e79b293dc89e4bfb4b9ae9714daca372729ba9
tiztrain <adamtolo20@hotmail.com>
2019-09-26 22:16:14 +1000

    -jdbc: updated string to insert values to db and tested -gradeprocessing: added CalculateGrade method

--- src/GradeProcessing.java
+++ src/GradeProcessing.java
@@ -22,6 +22,7 @@ public class GradeProcessing extends Application {
     private TextField cumulativeMark = new TextField();
     private TextField grade = new TextField();
 
+    private Button btCalculateRecord = new Button("Calculate Record");
     private Button btSearchRecord = new Button("Search for Record");
     private Button btInsertRecord = new Button("Insert Record");
     private Button btUpdateRecord = new Button("Update Record");
@@ -43,21 +44,22 @@ public class GradeProcessing extends Application {
         gridPane.add(id, 1, 0);
         gridPane.add(new Label("Name:"), 0, 1);
         gridPane.add(name, 1, 1);
-        gridPane.add(new Label("Quiz:"), 0, 2);
+        gridPane.add(new Label("Quiz mark:"), 0, 2);
         gridPane.add(quiz, 1, 2);
-        gridPane.add(new Label("A1:"), 0, 3);
+        gridPane.add(new Label("Assignment 1 mark:"), 0, 3);
         gridPane.add(a1, 1, 3);
-        gridPane.add(new Label("A2:"), 0, 4);
+        gridPane.add(new Label("Assignment 2 mark:"), 0, 4);
         gridPane.add(a2, 1, 4);
-        gridPane.add(new Label("Exam:"), 0, 5);
+        gridPane.add(new Label("Exam mark:"), 0, 5);
         gridPane.add(exam, 1, 5);
         gridPane.add(new Label("Cumulative Mark:"), 0, 6);
         gridPane.add(cumulativeMark, 1, 6);
         gridPane.add(new Label("Grade:"), 0, 7);
         gridPane.add(grade, 1, 7);
-        gridPane.add(btSearchRecord, 1, 8);
-        gridPane.add(btInsertRecord, 1, 9);
-        gridPane.add(btUpdateRecord, 1, 10);
+        gridPane.add(btCalculateRecord, 1, 9);
+        gridPane.add(btSearchRecord, 1, 10);
+        gridPane.add(btInsertRecord, 1, 11);
+        gridPane.add(btUpdateRecord, 1, 12);
 
         // Set properties for UI
         gridPane.setAlignment(Pos.CENTER);
@@ -66,18 +68,45 @@ public class GradeProcessing extends Application {
         quiz.setAlignment(Pos.BOTTOM_RIGHT);
         a1.setAlignment(Pos.BOTTOM_RIGHT);
         a2.setAlignment(Pos.BOTTOM_RIGHT);
-        quiz.setAlignment(Pos.BOTTOM_RIGHT);
-        cumulativeMark.setAlignment(Pos.BOTTOM_RIGHT);
         exam.setAlignment(Pos.BOTTOM_RIGHT);
         cumulativeMark.setEditable(false);
         grade.setEditable(false);
 
+        // Process events
+        btCalculateRecord.setOnAction(e -> CalculateGrade());
+
         // Create a scene and place it in the stage
-        Scene scene = new Scene(gridPane, 400, 350);
+        Scene scene = new Scene(gridPane, 400, 600);
         primaryStage.setTitle("GradeProcessing"); // Set title
         primaryStage.setScene(scene); // Place the scene in the stage
         primaryStage.show(); // Display the stage
 
+    }
+
+    private void CalculateGrade() {
+        // Get values from text fields
+        double quizMark = Double.parseDouble(quiz.getText());
+        double a1Mark = Double.parseDouble(a1.getText());
+        double a2Mark = Double.parseDouble(a2.getText());
+        double examMark = Double.parseDouble(exam.getText());
+        double totalMark;
+        String result = "";
+
+        totalMark = (quizMark * 0.05) + (a1Mark * 0.15) + (a2Mark * 0.2) + (examMark * 0.6);
+
+        if (totalMark < 50) {
+            result = "Fail";
+        } else if (totalMark >= 50 && totalMark < 65) {
+            result = "Pass";
+        } else if (totalMark >= 65 && totalMark < 75) {
+            result = "CR";
+        } else if (totalMark >= 75 && totalMark < 85) {
+            result = "DI";
+        } else
+            result = "HD";
+
+        cumulativeMark.setText(String.valueOf(totalMark));
+        grade.setText(result);
 
     }
 }
--- src/itc521_ass3_modules/Jdbc.java
+++ src/itc521_ass3_modules/Jdbc.java
@@ -1,8 +1,6 @@
 package itc521_ass3_modules;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Jdbc {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
@@ -15,5 +13,28 @@ public class Jdbc {
         Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1/gradeprocessing", "root", "admin");
         System.out.println("Successfully connected to Database");
 
+        // Create a statement
+        Statement statement = connection.createStatement();
+
+        // Hardcode variables for now
+        int id = 1234;
+        String name = "bob";
+        double quiz = 90;
+        double a1 = 75;
+        double a2 = 77;
+        double exam = 68;
+        double cumulativeMark = 79;
+        String grade = "D";
+
+
+//        String sql="";
+//        sql = "INSERT INTO Java2 " + "VALUES (" + id + ",'" + name + "'," + quiz + "," + a1 + "," + a2 + "," + exam + "," + cumulativeMark + ",'" + grade + "')";
+//        statement.execute(sql);
+//        sql ="INSERT INTO Java2 " + "VALUES (6111, 'Test', 90, 90, 90, 90, 90, 'HD')";
+//        statement.execute(sql);
+
+        // Execute a statement
+        ResultSet resultSet = statement.executeQuery("select * from Java2");
+        System.out.println("Inserted into database");
     }
 }
