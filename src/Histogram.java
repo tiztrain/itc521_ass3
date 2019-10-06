43443402892f1d4a0842c7acf7550dee519a2827
tiztrain <adamtolo20@hotmail.com>
2019-10-06 21:42:16 +1000

    -3 algorithms implemented but only run 1 type of sort

--- src/Histogram.java
+++ src/Histogram.java
@@ -52,151 +52,43 @@ public class Histogram extends Application {
 //        yAxis.setLabel("Y Axis");
 
         // creates the xy series
-        XYChart.Series selectionSort = new XYChart.Series();
-        selectionSort.setName("Selection Sort");
-        XYChart.Series insertionSort = new XYChart.Series();
-        insertionSort.setName("Insertion Sort");
-        XYChart.Series bubbleSort = new XYChart.Series();
-        bubbleSort.setName("Bubble Sort");
+        XYChart.Series selectionChart = new XYChart.Series();
+        selectionChart.setName("Selection Sort");
+        XYChart.Series insertionChart = new XYChart.Series();
+        insertionChart.setName("Insertion Sort");
+        XYChart.Series bubbleChart = new XYChart.Series();
+        bubbleChart.setName("Bubble Sort");
 
         // create bar chart and add in pane
-        barChartSelect.getData().addAll(selectionSort);
-        barChartInsert.getData().addAll(insertionSort);
-        barChartBubble.getData().addAll(bubbleSort);
+        barChartSelect.getData().addAll(selectionChart);
+        barChartInsert.getData().addAll(insertionChart);
+        barChartBubble.getData().addAll(bubbleChart);
 
         VBox vBox = new VBox();
         vBox.getChildren().addAll(barChartSelect, barChartInsert, barChartBubble);
 
+        // create tasks for different sorting methods
+        Runnable selSortAlgo = new SelectionSorting(data, selectionChart);
+        Runnable insSortAlgo = new InsertionSorting(data, insertionChart);
+        Runnable bubSortAlgo = new BubbleSorting(data, bubbleChart);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    int i;
-                    int runTime = 0;
-                    for (i = 0; i <= data.length - 1; i++) {
+        // create a thread for each sorting task
+        Thread selThread = new Thread(selSortAlgo);
+        Thread insThread = new Thread(insSortAlgo);
+        Thread bubThread = new Thread(bubSortAlgo);
 
-                        System.out.println("SELECTION RUNTIME: " + runTime);
-                        runTime++;
-
-                        int index = i;
-                        for (int j = i + 1; j < data.length; j++)
-                            if (data[j] < data[index])
-                                index = j;
-
-                        int smallerNumber = data[index];
-                        data[index] = data[i];
-                        data[i] = smallerNumber;
-
-                        // potentially need to add the display chart functionality here
-                        Platform.runLater(new Runnable() {
-                            @Override
-                            public void run() {
-                                for (int i = 0; i < DATA_SIZE; i++) {
-                                    System.out.println("selection data = " + data[i]);
-                                    selectionSort.getData().add(new XYChart.Data(String.valueOf(i), data[i]));
-                                }
-                            }
-                        });
-
-                        Thread.sleep(200);
-                    }
-                }//end outer for loop}//end selection sort
-                catch (InterruptedException ex) {
-                }
-            }
-        }).start();
-
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    int i;
-                    int j;
-                    int runTime = 0;
-                    int temp;
-                    for (j = 1; j < data.length - 1; j++) {
-
-                        System.out.println("    INSERT RUNTIME: " + runTime);
-                        runTime++;
-
-                        temp = data[j];
-                        i = j; // range 0 to j-1 is sorted
-                        while (i > 0 && data[i - 1] >= temp) {
-                            data[i] = data[i - 1];
-                            i--;
-                        }
-                        data[i] = temp;
-                        //} // end outer for loop
-
-                        // potentially need to add the display chart functionality here
-                        Platform.runLater(new Runnable() {
-                            @Override
-                            public void run() {
-                                for (int i = 0; i < DATA_SIZE; i++) {
-                                    System.out.println("    insertion data = " + data[i]);
-                                    insertionSort.getData().add(new XYChart.Data(String.valueOf(i), data[i]));
-                                }
-                            }
-                        });
-
-                        Thread.sleep(200);
-                    }
-                }//end outer for loop}//end selection sort
-                catch (InterruptedException ex) {
-                }
-            }
-        }).start();
-
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    int i;
-                    int j;
-                    int runTime = 0;
-                    int temp;
-                    for (i = data.length - 1; i > 0; i--) {
-
-                        System.out.println("        BUBBLE RUNTIME: " + runTime);
-                        runTime++;
-
-                        for (j = 0; j < i; j++) {
-                            if (data[j] > data[j + 1]) {
-                                temp = data[j];
-                                data[j] = data[j + 1];
-                                data[j + 1] = temp;
-                            }
-                        }// end inner for loop
-
-                        // potentially need to add the display chart functionality here
-                        Platform.runLater(new Runnable() {
-                            @Override
-                            public void run() {
-                                for (int i = 0; i < DATA_SIZE; i++) {
-                                    System.out.println("        bubble data = " + data[i]);
-                                    bubbleSort.getData().add(new XYChart.Data(String.valueOf(i), data[i]));
-                                }
-                            }
-                        });
-
-                        Thread.sleep(200);
-                    }
-                }//end outer for loop}//end selection sort
-                catch (InterruptedException ex) {
-                }
-            }
-        }).start();
+        // start threads
+        selThread.start();
+        insThread.start();
+        bubThread.start();
 
 
         // create scene and place on stage
         Scene scene = new Scene(vBox, 800, 1000);
 
-            primaryStage.setTitle("Multithreading for Sorting Methods");
-            primaryStage.setScene(scene);
-            primaryStage.show();
+        primaryStage.setTitle("Multithreading for Sorting Methods");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 
@@ -212,7 +104,148 @@ public class Histogram extends Application {
 
 }
 
+class SelectionSorting implements Runnable {
+    private int[] data;
+    private XYChart.Series sortChart;
+
+    public SelectionSorting(int[] d, XYChart.Series s) {
+        data = d;
+        sortChart = s;
+    }
+
+    @Override
+    public void run() {
+        try {
+            int i;
+            int runTime = 0;
+            for (i = 0; i <= data.length - 1; i++) {
+
+                System.out.println("SELECTION RUNTIME: " + runTime);
+                runTime++;
+
+                int index = i;
+                for (int j = i + 1; j < data.length; j++)
+                    if (data[j] < data[index])
+                        index = j;
+
+                int smallerNumber = data[index];
+                data[index] = data[i];
+                data[i] = smallerNumber;
+
+                // potentially need to add the display chart functionality here
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int i = 0; i < 30; i++) {
+                            System.out.println("selection data = " + data[i]);
+                            sortChart.getData().add(new XYChart.Data(String.valueOf(i), data[i]));
+                        }
+                    }
+                });
+
+                Thread.sleep(200);
+            }
+        }//end outer for loop}//end selection sort
+        catch (InterruptedException ex) {
+        }
+    }
+}
 
 
+class InsertionSorting implements Runnable {
+    private int[] data;
+    private XYChart.Series sortChart;
+
+    public InsertionSorting(int[] d, XYChart.Series s) {
+        data = d;
+        sortChart = s;
+    }
+
+    @Override
+    public void run() {
+        try {
+            int i;
+            int j;
+            int runTime = 0;
+            int temp;
+            for (j = 1; j < data.length - 1; j++) {
+
+                System.out.println("        INSERT RUNTIME: " + runTime);
+                runTime++;
+
+                temp = data[j];
+                i = j; // range 0 to j-1 is sorted
+                while (i > 0 && data[i - 1] >= temp) {
+                    data[i] = data[i - 1];
+                    i--;
+                }
+                data[i] = temp;
+                //} // end outer for loop
+
+                // potentially need to add the display chart functionality here
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int i = 0; i < 30; i++) {
+                            System.out.println("        insertion data = " + data[i]);
+                            sortChart.getData().add(new XYChart.Data(String.valueOf(i), data[i]));
+                        }
+                    }
+                });
+
+                Thread.sleep(200);
+            }
+        }//end outer for loop}//end selection sort
+        catch (InterruptedException ex) {
+        }
+    }
+}
 
 
+class BubbleSorting implements Runnable {
+    private int[] data;
+    private XYChart.Series sortChart;
+
+    public BubbleSorting(int[] d, XYChart.Series s) {
+        data = d;
+        sortChart = s;
+    }
+
+    @Override
+    public void run() {
+        try {
+            int i;
+            int j;
+            int runTime = 0;
+            int temp;
+            for (i = data.length - 1; i > 0; i--) {
+
+                System.out.println("                BUBBLE RUNTIME: " + runTime);
+                runTime++;
+
+                for (j = 0; j < i; j++) {
+                    if (data[j] > data[j + 1]) {
+                        temp = data[j];
+                        data[j] = data[j + 1];
+                        data[j + 1] = temp;
+                    }
+                }// end inner for loop
+
+                // potentially need to add the display chart functionality here
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int i = 0; i < 30; i++) {
+                            System.out.println("                bubble data = " + data[i]);
+                            sortChart.getData().add(new XYChart.Data(String.valueOf(i), data[i]));
+                        }
+                    }
+                });
+
+                Thread.sleep(200);
+            }
+        }//end outer for loop}//end selection sort
+        catch (InterruptedException ex) {
+        }
+    }
+}
